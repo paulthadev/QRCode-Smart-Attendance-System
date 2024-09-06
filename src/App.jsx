237673a600d2a0b6261c5760f29c4e40bc2ff3a8
fadayopaul <paulfadayo@yahoo.com>
2024-09-06 13:36:19 +0100

    chore: Add Tailwind CSS configuration and dependencies
    Co-Authored-By: Victor olabisi <oguntayovictor65@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="bg-gray-800 text-white p-12 rounded-lg shadow-lg items-center flex flex-col gap-2">
+        <h1 className="text-secondary font-bold">Hello</h1>
+        <button className="btn btn-primary">Button</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
