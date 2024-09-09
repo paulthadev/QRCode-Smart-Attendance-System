4f7367cb314e20e1a3881580612aa11b5668fea0
fadayopaul <paulfadayo@yahoo.com>
2024-09-09 14:17:48 +0100

    Add Spinner component

---
+++ src/component/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <section className={`flex items-center justify-center`}>
+      <div className="loading loading-spinner  bg-white"></div>
+    </section>
+  );
+}
+
+export default Spinner;
