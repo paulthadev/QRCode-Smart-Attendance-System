08e6cec604de78389d862c965184a43bc9ee16ba
fadayopaul <paulfadayo@yahoo.com>
2024-09-09 23:48:10 +0100

    feat: Update LandingPage with improved UI and button for student login
    This commit updates the LandingPage component to improve the user interface by adding a centered section with a welcome message and options for lecturers and students. It also includes a button for student login, providing a clear and intuitive way for students to access their class schedule. These changes enhance the functionality and user experience of the landing page.
    
    Co-Authored-By: Victor olabisi <oguntayovictor65@gmail.com>

--- src/page/LandingPage.jsx
+++ src/page/LandingPage.jsx
@@ -2,17 +2,26 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Welcome to our Registration Page</h1>
-      <Link to="/registerLecturer">
-        <button className="btn btn-primary">Register as Lecturer</button>
-      </Link>
-      <br />
+    <section className="h-[100vh] w-full grid place-items-center px-6 ">
+      <div>
+        <h1 className="text-center text-2xl font-bold text-black">
+          Welcome to AS track{" "}
+        </h1>
+        <h2 className="text-center text-lg text-black">
+          Are you a lecturer or a student{" "}
+        </h2>
+        <div className="flex gap-x-4 mt-4 justify-center">
+          <Link to="/registerLecturer">
+            <button className=" btn btn-primary">Lecturer</button>
+          </Link>
+          {/* <br /> */}
 
-      <Link to="/loginLecturer">
-        <button className="btn btn-secondary">Login as Lecturer</button>
-      </Link>
-    </div>
+          <Link to="/studentLogin">
+            <button className="btn  btn-secondary ">Student</button>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
