18ed259a01819e01382241d6f4d9ef0ded89127b
fadayopaul <paulfadayo@yahoo.com>
2024-09-09 14:43:12 +0100

    refactor: Update Input component to include name and value props
    This commit updates the Input component to include the name and value props. This change allows for better control and management of form inputs, making it easier to handle form submissions and data processing. The name prop is used for the input element's name attribute, while the value prop is used to set the initial value of the input. This enhancement improves the overall functionality and usability of the Input component.

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/prop-types */
-const Input = ({ label, type, placeholder, onChange }) => {
+const Input = ({ label, type, placeholder, onChange, value, name }) => {
   return (
     <div className="form-control">
-      <label htmlFor="" className="label">
+      <label htmlFor={name} className="label">
         <span className="label-text font-bold text-sm text-[#1E1E1E]">
           {label}
         </span>
       </label>
       <input
+        name={name}
+        id={name}
         type={type}
         onChange={onChange}
+        value={value}
         required
         placeholder={placeholder}
         className="input bg-white w-[100%]  text-black border-black border-[1px] focus:border-black  rounded-[0.375rem]"
