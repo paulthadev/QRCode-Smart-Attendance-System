69f3b0f07b7ee0583d69265f00c614e5b95ea92a
fadayopaul <paulfadayo@yahoo.com>
2024-09-10 01:42:42 +0100

    ```text feat: Add QRCodeModal component for displaying QR codes
    This commit adds a new component, QRCodeModal, which is responsible for displaying QR codes. The component uses the qrcode.react npm dependency to generate the QR code based on the provided data. This addition enhances the functionality of the application by allowing the display of QR codes for various purposes.

---
+++ src/component/QRCodeModal.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import { QRCodeCanvas } from "qrcode.react";
+
+const QRCodeModal = ({ data, onClose }) => {
+  return (
+    <dialog open className="modal">
+      <div className="modal-box modal-bottom sm:modal-middle w-11/12 max-w-5xl">
+        <p className="font-bold text-center text-lg sm:text-sm pb-2">QR Code</p>
+
+        <div className="flex justify-center">
+          {/* Generate the QR Code */}
+          <QRCodeCanvas value={data} size={256} />
+        </div>
+
+        <div className="modal-action">
+          <button className="btn" onClick={onClose}>
+            Close
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default QRCodeModal;
