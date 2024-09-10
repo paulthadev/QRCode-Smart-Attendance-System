f90c3e749c2dac51bb8decd519d2f905edef5314
fadayopaul <paulfadayo@yahoo.com>
2024-09-10 09:19:10 +0100

    feat: Update tailwind.config.js with new screen sizes and font families
    The `tailwind.config.js` file has been updated to include new screen sizes and font families. This change enhances the styling capabilities of the application by providing more options for responsive design and font selection.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "500px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4k": "2560px",
+    },
+    extend: {
+      colors: {},
+
+      fontFamily: {
+        sans: ['"Inter"', "sans-serif"],
+        "Josefin Sans": ["Josefin Sans"],
+        Poppins: ["Poppins"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
