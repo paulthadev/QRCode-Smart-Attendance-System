5c12d4214cf81aad4f082703af845637f511eda1
fadayopaul <paulfadayo@yahoo.com>
2024-09-10 14:36:59 +0100

    feat: Add course details to StudentLogin page
    This change adds the course details to the StudentLogin page. The course code, time, and lecture venue are extracted from the URL parameters and displayed on the page. This enhancement improves the user experience by providing students with easy access to their course information during the login process.

--- src/page/StudentLogin.jsx
+++ src/page/StudentLogin.jsx
@@ -1,9 +1,26 @@
+import { useLocation } from "react-router-dom";
 import Input from "../component/Input";
 
 const StudentLogin = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const courseCode = queryParams.get("courseCode");
+  const time = queryParams.get("time");
+  const lectureVenue = queryParams.get("lectureVenue");
+  const lat = queryParams.get("lat");
+  const lng = queryParams.get("lng");
+
   return (
     <section className="studentLogin h-[100vh] grid place-items-center ">
       <div className="bg-white px-6 py-4 md:px-24 md:py-12   rounded-xl">
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="text-[#000D46] text-lg font-bold">{courseCode}</p>
+            <p className="text-[#000D46] text-lg font-bold">{time}</p>
+            <p className="text-[#000D46] text-lg font-bold">{lectureVenue}</p>
+          </div>
+        </div>
         <form action="max-w-[50rem]">
           <h2 className="text-[2.5rem] text-[#000D46] text-center font-bold mb-8">
             ClassTrakka
@@ -22,4 +39,5 @@ const StudentLogin = () => {
     </section>
   );
 };
+
 export default StudentLogin;
